d90c3c3a132f77d5630473115c1a296fb0dbe9d3
Pierre <pierremarais67@gmail.com>
2024-02-11 15:40:46 +0100

    Added tests to ensure that the same url has the same hash and that different urls have different hashes

--- src/test/kotlin/pierremarais/unit/Base58ShorteningStrategyTest.kt
+++ src/test/kotlin/pierremarais/unit/Base58ShorteningStrategyTest.kt
@@ -1,7 +1,9 @@
 package pierremarais.unit
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import pierremarais.urlshortener.Base58Strategy
+import kotlin.test.assertNotEquals
 
 class Base58ShorteningStrategyTest {
     private val base58Shortening = Base58Strategy()
@@ -28,5 +30,34 @@ class Base58ShorteningStrategyTest {
         result.forEach {
             assert(base58Chars.contains(it))
         }
+    }
+
+    @Test
+    fun `shorten should return a different shortened string for different urls`() {
+        // Given
+        val url1 = "https://www.google.com/234982394087293847293472938472938472938472389472938472938472983423423423"
+        val url2 = "https://www.google.com/aaaaaaaaaaaaasssssssssssssssddddddddddddddddddddffffffffffffffffffffffffffffffffffff"
+        // When
+        val result = base58Shortening.shorten(url1)
+        val result2 = base58Shortening.shorten(url2)
+        // Then
+        assert(result.length <= 8)
+        assert(result2.length <= 8)
+
+        assertNotEquals(result, result2)
+    }
+
+    @Test
+    fun `shortening the same url twice should return the same shortened value`() {
+        // Given
+        val url = "https://www.google.com/234982394087293847293472938472938472938472389472938472938472983423423423"
+        // When
+        val result = base58Shortening.shorten(url)
+        val result2 = base58Shortening.shorten(url)
+        // Then
+        assert(result.length <= 8)
+        assert(result2.length <= 8)
+
+        assertEquals(result, result2)
     }
 }
