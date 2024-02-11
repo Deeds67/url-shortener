744309891a88da27c59494524554f85232bd5367
Pierre <pierremarais67@gmail.com>
2024-02-11 14:45:12 +0100

    Implemented base58 encoding the md5 hashed input

--- src/main/kotlin/pierremarais/urlshortener/ShorteningStrategy.kt
+++ src/main/kotlin/pierremarais/urlshortener/ShorteningStrategy.kt
@@ -1,11 +1,35 @@
 package pierremarais.urlshortener
 
+import java.math.BigInteger
+import java.security.MessageDigest
+
 interface ShorteningStrategy {
     fun shorten(value: String): String
 }
 
 class Base58Strategy: ShorteningStrategy {
+    private val base58Chars = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+
     override fun shorten(value: String): String {
-        return value.take(8)
+        val hash = md5(value)
+        val hashInt = BigInteger(1, hash)
+
+        val result = StringBuilder()
+        var num = hashInt
+        while (num > BigInteger.ZERO) {
+            val remainder = num.mod(BigInteger.valueOf(58))
+            result.insert(0, base58Chars[remainder.toInt()])
+            num /= BigInteger.valueOf(58)
+        }
+
+        val shortenedString = result.toString()
+
+        // Truncate the base58 string if it exceeds 8 characters
+        return if (shortenedString.length > 8) shortenedString.substring(0, 8) else shortenedString
+    }
+
+    private fun md5(input: String): ByteArray {
+        val digest = MessageDigest.getInstance("MD5")
+        return digest.digest(input.toByteArray())
     }
 }
