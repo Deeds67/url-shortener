889454cd6a4c6b38c4c1d1dda69856156eb215cd
Pierre <pierremarais67@gmail.com>
2024-02-11 16:02:37 +0100

    Added database info to app.conf

--- src/main/kotlin/pierremarais/AppConfig.kt
+++ src/main/kotlin/pierremarais/AppConfig.kt
@@ -2,10 +2,18 @@ package pierremarais
 
 import com.sksamuel.hoplite.ConfigLoader
 
-class AppConfig() {
+data class AppConfig(val database: DatabaseConfig) {
     companion object {
         fun fromConfigFile(): AppConfig {
             return ConfigLoader().loadConfigOrThrow("/application.conf")
         }
     }
 }
+
+data class DatabaseConfig(
+    val user: String,
+    val password: String,
+    val host: String,
+    val port: String,
+    val database: String
+)
