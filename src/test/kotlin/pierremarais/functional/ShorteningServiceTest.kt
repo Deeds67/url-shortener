c82f19f087c865997a24f16b9b4effdd56cceb16
Pierre <pierremarais67@gmail.com>
2024-02-11 15:50:56 +0100

    Shortening same urls gives same short url

--- src/test/kotlin/pierremarais/functional/ShorteningServiceTest.kt
+++ src/test/kotlin/pierremarais/functional/ShorteningServiceTest.kt
@@ -5,6 +5,7 @@ import pierremarais.urlshortener.Base58Strategy
 import pierremarais.urlshortener.InMemoryShortenedURLRepository
 import pierremarais.urlshortener.ShorteningServiceImpl
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
 import kotlin.test.assertTrue
 
@@ -45,5 +46,25 @@ class ShorteningServiceTest {
         assertNotEquals(shortURL1, shortURL2)
         assertTrue { result1.created }
         assertTrue { result2.created }
+    }
+
+    @Test
+    fun `shortening the same url should return the same short url`() {
+        // Given
+        val repository = InMemoryShortenedURLRepository()
+        val shorteningService = ShorteningServiceImpl(strategy, repository)
+
+        val url1 = "https://www.google.com/1"
+        val url2 = "https://www.google.com/1"
+        // When
+        val result1 = shorteningService.shorten(url1)
+        val result2 = shorteningService.shorten(url2)
+
+        val shortURL1 = result1.shortURL
+        val shortURL2 = result2.shortURL
+        // Then
+        assertEquals(shortURL1, shortURL2)
+        assertTrue { result1.created }
+        assertFalse { result2.created }
     }
 }
